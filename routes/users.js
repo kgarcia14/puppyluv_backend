4d0174fee1c71bbdeb085ece1dc628e83fa14a0f
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-03-28 20:15:08 -0400

    refactored backend to take in the user_nickname from auth_0

--- routes/users.js
+++ routes/users.js
@@ -1,2 +1,34 @@
+const express = require('express'),
+    router = express.Router(),
+    usersModel = require('../models/usersModel');
 
+router.get('/', async (req, res) => {
+    const usersData = await usersModel.getAll();
 
+    res.json(usersData).status(200);
+});
+
+router.get('/:user_nickname', async (req, res) => {
+    const { user_nickname } = req.params;
+    console.log(req.params)
+    const user = await usersModel.getByUserNickname(user_nickname);
+    
+    if (user) {
+        res.json(user).status(200);
+    } else {
+        res.status(404).send(`No user by that user_nickname here.`)
+    }
+});
+
+router.post('/add', async (req, res) => {
+    const { first_name, last_name, age, gender, city, zipcode, about_us, user_img, user_nickname, pet_name, pet_breed, pet_age, pet_personality, pet_img } = req.body;
+    console.log('reqBody: ', req.body);
+    const response = await usersModel.addEntry(first_name, last_name, age, gender, city, zipcode, about_us, user_img, user_nickname, pet_name, pet_breed, pet_age, pet_personality, pet_img);
+    if (response.rowCount >= 1) {
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(500)
+    }
+});
+
+module.exports = router;
