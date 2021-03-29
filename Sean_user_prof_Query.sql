4335f551835fba4c42d6ca79e4235c6615a4601a
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-03-28 20:22:35 -0400

    refactored sql query in order to link auth0 to back end

---
+++ Sean_user_prof_Query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user_profile (
+id serial PRIMARY KEY,
+first_name text,
+last_name text,
+age integer,
+gender text,
+city text,
+zipcode integer,
+about_us text,
+user_img VARCHAR(200),
+user_nickname VARCHAR(200),
+pet_name VARCHAR(200),
+pet_breed VARCHAR(200),
+pet_age integer,
+pet_personality VARCHAR(200),
+pet_img VARCHAR (500)
+);
+
+DROP TABLE user_profile;
